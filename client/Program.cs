74b0fd82119ef55f69ffbb96fc0e719beebe1647
Ingrid <ingridsandev@gmail.com>
2020-01-13 22:47:46 +0000

    Add project files.

---
+++ client/Program.cs
@@ -0,0 +1,53 @@
+﻿using Calculator;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new CalculatorService.CalculatorServiceClient(channel);
+
+            Console.WriteLine("Which is the FIRST value for your operation? ");
+            var firstValue = Console.ReadLine();
+
+            Console.WriteLine("Which is the SECOND value for your operation? ");
+            var secondValue = Console.ReadLine();
+
+            Console.WriteLine("Inform the operation:");
+            Console.WriteLine("  + = Addition ");
+            Console.WriteLine("  - = Subtraction ");
+            Console.WriteLine("  * = Multiplication ");
+            Console.WriteLine("  / = Division ");
+
+            var operation = Console.ReadLine();
+
+            var request = new CalculatorRequest()
+            {
+                FirstValue = Convert.ToInt32(firstValue),
+                SecondValue = Convert.ToInt32(secondValue),
+                Operation = operation
+            };
+
+            var response = client.Calculator(request);
+
+            Console.WriteLine(response.Result);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ server/CalculatorServiceImplementation.cs
@@ -0,0 +1,33 @@
+﻿using Calculator;
+using Grpc.Core;
+using System.Threading.Tasks;
+using static Calculator.CalculatorService;
+
+namespace server
+{
+    public class CalculatorServiceImplementation : CalculatorServiceBase
+    {
+        public override Task<CalculatorResponse> Calculator(CalculatorRequest request, ServerCallContext context)
+        {
+            var result = 0;
+
+            switch (request.Operation)
+            {
+                case "+":
+                    result = request.FirstValue + request.SecondValue;
+                    break;
+                case "-":
+                    result = request.FirstValue - request.SecondValue;
+                    break;
+                case "*":
+                    result = request.FirstValue * request.SecondValue;
+                    break;
+                case "/":
+                    result = request.FirstValue / request.SecondValue;
+                    break;
+            }
+
+            return Task.FromResult(new CalculatorResponse() { Result = result, Request = request });
+        }
+    }
+}
